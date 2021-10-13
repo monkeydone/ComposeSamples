8b635ca023a488f3b8c53ac336c866c69a47f31f
lijunjie <lijunjie@windimg.com>
2021-10-13 18:22:59 +0800

    增加动画的一些demo

--- app/src/main/java/com/a/compose/AnimationDemo.kt
+++ app/src/main/java/com/a/compose/AnimationDemo.kt
@@ -1,25 +1,31 @@
 package com.a.compose
 
+import android.graphics.Paint
 import androidx.compose.animation.*
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 
@@ -90,7 +96,10 @@ fun ColorDemo() {
     var colorFlag by remember {
         mutableStateOf(true)
     }
-    val backgroundColor by animateColorAsState(targetValue = if(colorFlag) Color.Black else Color.Red)
+    val backgroundColor by animateColorAsState(targetValue = if(colorFlag) Color.Black else Color.Red,
+    keyframes {
+        durationMillis = 5000
+    })
 
     Box(modifier = Modifier
         .size(100.dp)
@@ -189,13 +198,13 @@ fun AnimatedContentDemoV2() {
                                 keyframes {
                                     // Expand horizontally first.
                                     IntSize(targetSize.width, initialSize.height) at 150
-                                    durationMillis = 300
+                                    durationMillis = 3000
                                 }
                             } else {
                                 keyframes {
                                     // Shrink vertically first.
                                     IntSize(initialSize.width, targetSize.height) at 150
-                                    durationMillis = 300
+                                    durationMillis = 3000
                                 }
                             }
                         }
@@ -246,6 +255,202 @@ fun CrossfadeDemo() {
 
 }
 
+@Composable
+fun AnimatableDemo() {
+    var enabled by remember {
+        mutableStateOf(true)
+    }
+    val color = remember { Animatable(Color.Gray) }
+    LaunchedEffect(enabled) {
+        color.animateTo(if (enabled) Color.Green else Color.Red)
+    }
+    Box(
+        Modifier
+            .size(100.dp)
+            .background(color.value))
+    
+}
+
+enum class BoxState { Collapsed, Expanded }
+
+@Composable
+fun AnimatingBoxDemo() {
+    var boxState by remember{
+        mutableStateOf(BoxState.Collapsed)
+    }
+    var transitionData =  updateTransitionData(boxState)
+    // UI tree
+    Box(
+        modifier = Modifier
+            .background(transitionData.color)
+            .size(transitionData.size)
+            .clickable {
+                boxState = if (boxState == BoxState.Collapsed) BoxState.Expanded else BoxState.Collapsed
+            }
+    ){
+       Text("color:${transitionData.size},dp:${transitionData.color.toArgb()}")
+    }
+}
+
+// Holds the animation values.
+private class TransitionData(
+    color: State<Color>,
+    size: State<Dp>
+) {
+    val color by color
+    val size by size
+}
+
+// Create a Transition and return its animation values.
+@Composable
+private fun updateTransitionData(boxState: BoxState): TransitionData {
+    val transition = updateTransition(boxState)
+    val color = transition.animateColor { state ->
+        when (state) {
+            BoxState.Collapsed -> Color.Gray
+            BoxState.Expanded -> Color.Red
+        }
+    }
+    val size = transition.animateDp { state ->
+        when (state) {
+            BoxState.Collapsed -> 64.dp
+            BoxState.Expanded -> 128.dp
+        }
+    }
+    return remember(transition) { TransitionData(color, size) }
+}
+
+@Composable
+fun CircleAnimDemo() {
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val size by infiniteTransition.animateFloat(
+        initialValue = 100f,
+        targetValue = 200f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000,easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    Box(
+        Modifier
+            .size(size.dp)
+            .background(color))
+}
+enum class SurfaceState { Pressed, Released }
+
+@Preview
+@Composable
+fun PressedSurface() {
+
+    val (pressed, onPress) = remember { mutableStateOf(false) }
+    val transition = updateTransition(
+        targetState = if (pressed) SurfaceState.Pressed else SurfaceState.Released
+    )
+
+    val width by transition.animateDp { state ->
+        when (state) {
+            SurfaceState.Released -> 20.dp
+            SurfaceState.Pressed -> 50.dp
+        }
+    }
+    val surfaceColor by transition.animateColor { state ->
+        when (state) {
+            SurfaceState.Released -> Color.Blue
+            SurfaceState.Pressed -> Color.Red
+        }
+    }
+    val selectedAlpha by transition.animateFloat { state ->
+        when (state) {
+            SurfaceState.Released -> 0.5f
+            SurfaceState.Pressed -> 1f
+        }
+    }
+
+    Surface(
+        color = surfaceColor.copy(alpha = selectedAlpha),
+        modifier = Modifier
+            .toggleable(value = pressed, onValueChange = onPress)
+            .size(height = 200.dp, width = width)
+    ){}
+}
+
+@Preview
+@OptIn(ExperimentalAnimationApi::class)
+@ExperimentalMaterialApi
+@Composable
+fun TransitionDemo() {
+    var selected by remember { mutableStateOf(false) }
+// Animates changes when `selected` is changed.
+    val transition = updateTransition(selected)
+    val borderColor by transition.animateColor { isSelected ->
+        if (isSelected) Color.Magenta else Color.White
+    }
+    val elevation by transition.animateDp { isSelected ->
+        if (isSelected) 10.dp else 2.dp
+    }
+    Surface(
+        onClick = { selected = !selected },
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(2.dp, borderColor),
+        elevation = elevation
+    ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)) {
+            Text(text = "Hello, world!")
+            // AnimatedVisibility as a part of the transition.
+            transition.AnimatedVisibility(
+                visible = { targetSelected -> targetSelected },
+                enter = expandVertically(),
+                exit = shrinkVertically()
+            ) {
+                Text(text = "It is fine today.")
+            }
+            // AnimatedContent as a part of the transition.
+            transition.AnimatedContent { targetState ->
+                if (targetState) {
+                    Text(text = "Selected")
+                } else {
+                    Icon(imageVector = Icons.Default.Phone, contentDescription = "Phone")
+                }
+            }
+        }
+    }
+}
+@Composable
+fun FloatValueDemo() {
+    var enabled by remember {
+        mutableStateOf(true)
+    }
+    val alpha: Float by animateFloatAsState(if (enabled) 1f else 0.5f,
+    keyframes {
+        durationMillis = 4000
+    })
+    Box(
+        Modifier
+            .fillMaxSize()
+            .graphicsLayer(alpha = alpha)
+
+            .background(Color.Red)
+            .clickable {
+                enabled = !enabled
+            }
+
+    ) {
+        Text(text = "${alpha}")
+    }
+
+}
 
 @ExperimentalMaterialApi
 @OptIn(ExperimentalAnimationApi::class)
@@ -254,12 +459,24 @@ fun CrossfadeDemo() {
 fun AnimationDemo() {
     LazyColumn {
         item {
-            Text("Animation Demo", modifier = Modifier
+            Text("Animation Demo",textAlign = TextAlign.Center, modifier = Modifier
                 .fillMaxWidth()
                 .background(Color.Green))
         }
         item{
             var extended by remember { mutableStateOf(true)}
+            AnimatingBoxDemo()
+            Divider(Modifier.height(2.dp))
+            CircleAnimDemo()
+            Divider(Modifier.height(2.dp))
+            PressedSurface()
+            Divider(Modifier.height(2.dp))
+            TransitionDemo()
+            Divider(Modifier.height(2.dp))
+            AnimatableDemo()
+            Divider(Modifier.height(2.dp))
+            FloatValueDemo()
+            Divider(Modifier.height(2.dp))
             CrossfadeDemo()
             Divider(Modifier.height(2.dp))
             ContentSizeDemo()
