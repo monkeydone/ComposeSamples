231cad0379570caeb25ef8e9ef2e2dd59c96328b
lijunjie <lijunjie@windimg.com>
2021-10-13 18:37:43 +0800

    增加动画的一些demo

--- app/src/main/java/com/a/compose/AnimationDemo.kt
+++ app/src/main/java/com/a/compose/AnimationDemo.kt
@@ -3,10 +3,8 @@ package com.a.compose
 import android.graphics.Paint
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.selection.toggleable
@@ -19,15 +17,21 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -256,6 +260,34 @@ fun CrossfadeDemo() {
 }
 
 @Composable
+fun GestureDemo() {
+    val offset = remember { Animatable(Offset(0f, 0f), Offset.VectorConverter) }
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .pointerInput(Unit) {
+                coroutineScope {
+                    while (true) {
+                        // Detect a tap event and obtain its position.
+                        val position = awaitPointerEventScope {
+                            awaitFirstDown().position
+                        }
+                        launch {
+                            // Animate to the tap position.
+                            offset.animateTo(position)
+                        }
+                    }
+                }
+            }
+    ) {
+        Text(text="Text",modifier = Modifier.border(BorderStroke(2.dp,Color.Blue)).offset { offset.value.toIntOffset() })
+    }
+}
+
+private fun Offset.toIntOffset() = IntOffset(x.roundToInt(), y.roundToInt())
+
+
+@Composable
 fun AnimatableDemo() {
     var enabled by remember {
         mutableStateOf(true)
@@ -465,6 +497,8 @@ fun AnimationDemo() {
         }
         item{
             var extended by remember { mutableStateOf(true)}
+            GestureDemo()
+            Divider(Modifier.height(2.dp))
             AnimatingBoxDemo()
             Divider(Modifier.height(2.dp))
             CircleAnimDemo()
