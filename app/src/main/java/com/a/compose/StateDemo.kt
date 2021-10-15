92eed0b0c8eeeabc3827d0488048444c84566dd7
lijunjie <lijunjie@windimg.com>
2021-10-15 15:09:50 +0800

    增加对话框

--- app/src/main/java/com/a/compose/StateDemo.kt
+++ app/src/main/java/com/a/compose/StateDemo.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -112,7 +114,7 @@ fun ListTestV3(viewModel: TodoViewModel) {
             }
             .background(Color.Red)) {
         items(items = list2) { i ->
-            Text("Item ${i.task} ${i.id} $count",modifier= Modifier.background(Color.Green))
+            Text("Item ${i.task} ${i.id} $count",modifier= Modifier.clip(CircleShape).background(Color.Green))
         }
     }
 }
@@ -307,6 +309,7 @@ fun TodoScreen(viewModel: TodoViewModel) {
     }
 
 }
+
 
 
 @Composable
