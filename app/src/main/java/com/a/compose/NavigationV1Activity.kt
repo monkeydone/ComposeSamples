5b9864bbe34a19373292c92b98487660359c06f2
lijunjie <lijunjie@windimg.com>
2021-10-15 17:37:26 +0800

    增加导航页面

---
+++ app/src/main/java/com/a/compose/NavigationV1Activity.kt
@@ -0,0 +1,128 @@
+package com.a.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.a.compose.ui.theme.ComposeTheme
+
+class NavigationV1 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    MainApp()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MainApp() {
+    val allScreens = RallyScreen.values().toList()
+    var currentScreen by remember { mutableStateOf(RallyScreen.Overview) }
+    Scaffold(
+        topBar = {
+            RallyTabRow(
+                allScreens = allScreens,
+                onTabSelected = { screen -> currentScreen = screen },
+                currentScreen = currentScreen
+            )
+        }
+    ) { innerPadding ->
+        Box(Modifier.padding(innerPadding).background(Color.Blue).fillMaxSize()) {
+            currentScreen.content(
+                onScreenChange = { screen ->
+                    currentScreen = RallyScreen.valueOf(screen)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun RallyTabRow(
+    allScreens: List<RallyScreen>,
+    onTabSelected: (RallyScreen) -> Unit,
+    currentScreen: RallyScreen
+) {
+    Surface(
+        Modifier
+            .height(56.dp)
+            .fillMaxWidth()
+    ) {
+        Row(Modifier.selectableGroup()) {
+            allScreens.forEach { screen ->
+                val bg = if(currentScreen == screen) Color.Blue else Color.Red
+                Text(text = screen.name,modifier = Modifier.background(bg).clickable {
+                   onTabSelected(screen)
+                })
+//                RallyTab(
+//                    text = screen.name,
+//                    icon = screen.icon,
+//                    onSelected = { onTabSelected(screen) },
+//                    selected = currentScreen == screen
+//                )
+            }
+        }
+    }
+}
+
+@Composable
+fun EmptyView(name: String) {
+    Text(text = name)
+}
+
+
+enum class RallyScreen(
+    name: String,
+    val body: @Composable ((String) -> Unit) -> Unit
+) {
+    Overview(
+        name = "Overview",
+        body = { com.a.compose.EmptyView(name = "Overview")}
+    ),
+    Accounts(
+        name = "Account",
+        body = { com.a.compose.EmptyView(name = "Account") }
+    ),
+    Bills(
+        name = "Bills",
+        body = { com.a.compose.EmptyView(name = "Bills") }
+    );
+
+    @Composable
+    fun content(onScreenChange: (String) -> Unit) {
+        body(onScreenChange)
+    }
+
+    companion object {
+        fun fromRoute(route: String?): RallyScreen =
+            when (route?.substringBefore("/")) {
+                Accounts.name -> Accounts
+                Bills.name -> Bills
+                Overview.name -> Overview
+                null -> Overview
+                else -> throw IllegalArgumentException("Route $route is not recognized.")
+            }
+    }
+}
