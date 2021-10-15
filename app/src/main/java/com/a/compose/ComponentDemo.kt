c7820c920ec00c3c8278181456834c8e762880ce
lijunjie <lijunjie@windimg.com>
2021-10-15 17:07:47 +0800

    增加对话框

--- app/src/main/java/com/a/compose/ComponentDemo.kt
+++ app/src/main/java/com/a/compose/ComponentDemo.kt
@@ -9,19 +9,18 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.a.compose.ui.theme.ComposeTheme
+import java.util.*
 
 
 @ExperimentalAnimationApi
@@ -37,9 +36,22 @@ fun ComponentDemo() {
 @ExperimentalAnimationApi
 @Composable
 fun ComponentListDemo() {
+    var showDialog by remember { mutableStateOf(false) }
+    val alertMessage = "Heads up, you've used up 90% of your Shopping budget for this month."
+
     LazyColumn() {
         item{
             AnimatedIconRow()
+            if (showDialog) {
+                RallyAlertDialog(
+                    onDismiss = {
+                        showDialog = false
+                    },
+                    bodyText = alertMessage,
+                    buttonText = "Dismiss".uppercase(Locale.getDefault())
+                )
+            }
+            Text("显示对话框",modifier = Modifier.clickable { showDialog = true })
         }
 
     }
@@ -59,6 +71,7 @@ fun AnimatedIconRow(
     val enter = remember { fadeIn(animationSpec = TweenSpec(3000, easing = FastOutLinearInEasing)) }
     val exit = remember { fadeOut(animationSpec = TweenSpec(1000, easing = FastOutSlowInEasing)) }
     Box(modifier.size(50.dp).background(Color.Green).clickable {
+
         if(todoIcon == TodoIcon.Done) TodoIcon.Event else TodoIcon.Done
     }) {
         AnimatedVisibility(
@@ -68,5 +81,36 @@ fun AnimatedIconRow(
         ) {
             Icon(todoIcon.imageVector,contentDescription = "")
         }
+    }
+}
+
+
+@Composable
+fun RallyAlertDialog(
+    onDismiss: () -> Unit,
+    bodyText: String,
+    buttonText: String
+) {
+    ComposeTheme {
+        AlertDialog(
+            onDismissRequest = onDismiss,
+            text = { Text(bodyText) },
+            buttons = {
+                Column {
+                    Divider(
+                        Modifier.padding(horizontal = 12.dp),
+                        color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+                    )
+                    TextButton(
+                        onClick = onDismiss,
+                        shape = RectangleShape,
+                        contentPadding = PaddingValues(16.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(buttonText)
+                    }
+                }
+            }
+        )
     }
 }
