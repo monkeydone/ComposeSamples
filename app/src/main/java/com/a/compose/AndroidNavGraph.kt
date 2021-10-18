7d53e42c1d4b115afd36943cd580e053f3c4cdeb
lijunjie <lijunjie@windimg.com>
2021-10-18 13:22:41 +0800

    使用navigation导航

---
+++ app/src/main/java/com/a/compose/AndroidNavGraph.kt
@@ -0,0 +1,195 @@
+package com.a.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.a.compose.ui.theme.ComposeTheme
+import kotlinx.coroutines.launch
+
+class AndroidNavGraph : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    val scaffoldState = rememberScaffoldState()
+    val navController = rememberNavController()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = {
+           LeftMenu()
+        }
+    ) {
+        JetnewsNavGraph(
+            navController = navController,
+            scaffoldState = scaffoldState
+        )
+    }
+
+}
+
+object MainDestinations {
+    const val HOME_ROUTE = "home"
+    const val INTERESTS_ROUTE = "interests"
+    const val ARTICLE_ROUTE = "post"
+    const val ARTICLE_ID_KEY = "postId"
+}
+
+class MainActions(navController: NavHostController) {
+    val navigateToArticle: (String) -> Unit = { postId: String ->
+        navController.navigate("${MainDestinations.ARTICLE_ROUTE}/$postId")
+    }
+    val upPress: () -> Unit = {
+        navController.navigateUp()
+    }
+}
+
+
+
+@Composable
+fun JetnewsNavGraph(
+    navController: NavHostController = rememberNavController(),
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    startDestination: String = MainDestinations.HOME_ROUTE
+) {
+    val actions = remember(navController) { MainActions(navController) }
+    val coroutineScope = rememberCoroutineScope()
+    val openDrawer: () -> Unit = { coroutineScope.launch { scaffoldState.drawerState.open() } }
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(MainDestinations.HOME_ROUTE) {
+            Text(MainDestinations.HOME_ROUTE)
+        }
+        composable(MainDestinations.INTERESTS_ROUTE) {
+            Text(MainDestinations.INTERESTS_ROUTE)
+        }
+        composable("${MainDestinations.ARTICLE_ROUTE}/{${MainDestinations.ARTICLE_ID_KEY}") { backStackEntry ->
+            Text(MainDestinations.ARTICLE_ROUTE)
+        }
+    }
+}
+
+
+@Composable
+fun LeftMenu() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Spacer(Modifier.height(24.dp))
+        Divider(color = MaterialTheme.colors.onSurface.copy(alpha = .2f))
+        DrawerButton(
+            icon = Icons.Filled.Home,
+            label = stringResource(id = R.string.edit),
+            isSelected = false,
+            action = {
+//                navigateToHome()
+//                closeDrawer()
+            }
+        )
+
+        DrawerButton(
+            icon = Icons.Filled.Edit,
+            label = stringResource(id = R.string.app_name),
+            isSelected = true,
+            action = {
+//                navigateToInterests()
+//                closeDrawer()
+            }
+        )
+
+
+    }
+}
+
+@Composable
+private fun DrawerButton(
+    icon: ImageVector,
+    label: String,
+    isSelected: Boolean,
+    action: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val colors = MaterialTheme.colors
+    val imageAlpha = if (isSelected) {
+        1f
+    } else {
+        0.6f
+    }
+    val textIconColor = if (isSelected) {
+        colors.primary
+    } else {
+        colors.onSurface.copy(alpha = 0.6f)
+    }
+    val backgroundColor = if (isSelected) {
+        colors.primary.copy(alpha = 0.12f)
+    } else {
+        Color.Transparent
+    }
+
+    val surfaceModifier = modifier
+        .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+        .fillMaxWidth()
+    Surface(
+        modifier = surfaceModifier,
+        color = backgroundColor,
+        shape = MaterialTheme.shapes.small
+    ) {
+        TextButton(
+            onClick = action,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    imageVector = icon,
+                    contentDescription = null, // decorative
+                    colorFilter = ColorFilter.tint(textIconColor),
+                    alpha = imageAlpha
+                )
+                Spacer(Modifier.width(16.dp))
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.body2,
+                    color = textIconColor
+                )
+            }
+        }
+    }
+}
