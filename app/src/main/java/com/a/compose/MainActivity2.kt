e4bcbab1139b9ff28ba30a183975fc82a7c6126a
lijunjie <lijunjie@windimg.com>
2021-10-20 14:10:45 +0800

    在菜单中使用intent跳转

--- app/src/main/java/com/a/compose/MainActivity2.kt
+++ app/src/main/java/com/a/compose/MainActivity2.kt
@@ -2,7 +2,14 @@ package com.a.compose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +17,55 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val container = findViewById<ComposeView>(R.id.compose_view)
         container.setContent {
-            ShapeDemo()
+            Column() {
+                ShapeDemo()
+                val context = """
+                    private fun PlantDescription(description: String) {
+                        // Remembers the HTML formatted description. Re-executes on a new description
+                        val htmlDescription = remember(description) {
+                            HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+                        }
+
+                        // Displays the TextView on the screen and updates with the HTML description when inflated
+                        // Updates to htmlDescription will make AndroidView recompose and update the text
+                        AndroidView(
+                            factory = { context ->
+                                TextView(context).apply {
+                                    movementMethod = LinkMovementMethod.getInstance()
+                                }
+                            },
+                            update = {
+                                it.text = htmlDescription
+                            }
+                        )
+                    }
+                """.trimIndent()
+
+                PlantDescription(context)
+
+            }
         }
     }
+}
+
+
+@Composable
+private fun PlantDescription(description: String) {
+    // Remembers the HTML formatted description. Re-executes on a new description
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    // Displays the TextView on the screen and updates with the HTML description when inflated
+    // Updates to htmlDescription will make AndroidView recompose and update the text
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDescription
+        }
+    )
 }
