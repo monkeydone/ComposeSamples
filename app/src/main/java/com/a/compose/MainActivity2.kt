47517e280a69654805d44d1245c65e5e04d979c6
lijunjie <lijunjie@windimg.com>
2021-10-20 14:02:17 +0800

    在菜单中使用intent跳转

--- app/src/main/java/com/a/compose/MainActivity2.kt
+++ app/src/main/java/com/a/compose/MainActivity2.kt
@@ -2,10 +2,15 @@ package com.a.compose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.ui.platform.ComposeView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val container = findViewById<ComposeView>(R.id.compose_view)
+        container.setContent {
+            ShapeDemo()
+        }
     }
 }
